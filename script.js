c83e1e188de191b00933d784303830a697f94193
Jimenac5 <Jimenacerda03@gmail.com>
2025-03-05 18:45:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ let quote = ["I never lose I either win or learn - Nelson Mandela""Believe you c
 ""In the middle of every difficulty lies opportunity.= - Albert Einstein""Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.” – Martin Luther King Jr."];
 function displayQuote(){
   let randomQuoteIndex = Math.floor(Math.random()*quote.length);
-  let myQuote=quote[]
+  let myQuote=quote[randomQuoteIndex];
+  letmoodDivElement= document.getElementById("myQuote");
+  moodDivElement.innerHTML=myQuote;
+}
