06639d2c4791f207fde62a0a44ee8d2424695159
Jimenac5 <Jimenacerda03@gmail.com>
2025-03-05 19:41:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let quote = ["Be Happy", " Keep moving", "Never Quit", " Show true kindness"];
 function displayQuote(){
   let randomQuoteIndex = Math.floor(Math.random()*quote.length);
   let myQuote=quote[randomQuoteIndex];
-  letmoodDivElement= document.getElementById("myQuote");
-  moodDivElement.innerHTML=myQuote;
+  letquoteDivElement= document.getElementById("myQuote");
+  quoteDivElement.innerHTML=myQuote;
 }
