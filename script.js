ea62dc7d509508282a42dc53884d751e7b8bb48c
Jimenac5 <Jimenacerda03@gmail.com>
2025-03-05 21:32:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let quote = [
 ];
 function displayQuote(){
   let randomQuoteIndex = Math.floor(Math.random()*quote.length);
-  let myQuote=quote[randomMoodIndex];
+  let myQuote=quote[randomQuoteIndex];
   let quoteDivElement= document.getElementById("myQuote");
   quoteDivElement.innerHTML=myQuote;
 }
