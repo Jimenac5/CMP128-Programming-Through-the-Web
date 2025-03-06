bd87e54bfc125796cd925808f333f29eff34e9b1
Jimenac5 <Jimenacerda03@gmail.com>
2025-03-05 21:22:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let quote = [
   "Sad"
 ];
 function displayQuote(){
-  let randomQuoteIndex = Math.floor(Math.random()quote.length);
+  let randomQuoteIndex = Math.floor(Math.random()*quote.length);
   let myQuote=quote[randomMoodIndex];
   let quoteDivElement= document.getElementById("myQuote");
   quoteDivElement.innerHTML=myQuote;
