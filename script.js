36c2a974b98f5c4f9bdb3444429b24cbb0d5a9e0
Jimenac5 <Jimenacerda03@gmail.com>
2025-03-05 21:46:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let quote = [
-  "Happy",
-  "Nuetral",
-  "Sad"
+  "I never loose I either win or learn-Nelson Mandela",
+  "All our dreams can come true, if we have the courage to pursue them-Walt Disney",
+  "It always seems impossible until it's done-Nelosn Mandela"
+  "Either you run the day, or the day runs you-Jim Rohn"
 ];
 function displayQuote(){
   let randomQuoteIndex = Math.floor(Math.random()*quote.length);
