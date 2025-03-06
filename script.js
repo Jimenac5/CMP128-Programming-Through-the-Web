2d78edf00eabed746317850cff6755edde16cb6c
Jimenac5 <Jimenacerda03@gmail.com>
2025-03-05 21:57:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let quote = [
   "I never loose I either win or learn-Nelson Mandela",
   "All our dreams can come true, if we have the courage to pursue them-Walt Disney",
-  "It always seems impossible until it's done-Nelosn Mandela"
+  "It always seems impossible until it's done-Nelosn Mandela",
   "Either you run the day, or the day runs you-Jim Rohn"
 ];
 function displayQuote(){
